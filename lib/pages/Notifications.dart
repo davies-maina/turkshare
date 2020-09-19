e955a1025d881e14c71276c3c9dd780e006a2e5f
Davies Maina <daviesmaina@outlook.com>
2020-09-19 12:33:01 +0300

    reusable appbar +testing progress indicator

--- lib/pages/Notifications.dart
+++ lib/pages/Notifications.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:turkshare/widgets/header.dart';
 
 class Notifications extends StatefulWidget {
   @override
@@ -7,7 +8,17 @@ class Notifications extends StatefulWidget {
 
 class _NotificationsState extends State<Notifications> {
   @override
-  Widget build(context) {
-    return Text("Notifications");
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: header(context,currentPageTitle: 'Notifications'),
+    );
   }
 }
+
+class NotificationsItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context,{bool isAppTitle=false,String currentPageTitle,hideBackButton=false}) {
+  return AppBar(
+  iconTheme:IconThemeData(
+    color: Colors.white,
+
+  ),
+    automaticallyImplyLeading: hideBackButton ? false:true,
+    title: Text(
+        isAppTitle ?'Turkshare':currentPageTitle,
+        style: TextStyle(
+            color: Colors.white,
+            fontFamily: isAppTitle ? 'Signatra' : '',
+            fontSize: isAppTitle ? 45.0 : 22.0
+      ),
+
+      overflow: TextOverflow.ellipsis,
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
